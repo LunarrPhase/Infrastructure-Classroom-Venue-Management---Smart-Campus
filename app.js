c05b95f1d27a79099f8885c8b504d79694cbc08d
LunarrPhase <NewmanClaire6@gmail.com>
2024-08-13 21:11:31 +0200

    commit folders

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.static("src"));
+
+app.listen(app.get("port"), function(){
+
+    console.log(`Example app listening at http://localhost:${port}`);
+});
